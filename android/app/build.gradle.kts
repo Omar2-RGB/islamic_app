0781071706304e933871b045a09cc3c20081db97
Omar <blymtjr177@gmail.com>
2026-08-10 21:19:29 +0300

    fix: ترقية compileSdk و targetSdk إلى 36 لتوافق الحزم الحديثة

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.islamic_app"
-    compileSdk = 35 // 💡 تم التعديل هنا لفرض الإصدار 34 لتوافق المكتبات
+    compileSdk = 36  // 💡 تم التعديل هنا لفرض الإصدار 34 لتوافق المكتبات
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -18,7 +18,7 @@ android {
     defaultConfig {
         applicationId = "com.example.islamic_app"
         minSdk = flutter.minSdkVersion
-        targetSdk = 35 // 💡 تم التعديل هنا أيضاً ليطابق الـ compileSdk
+        targetSdk = 36 // 💡 تم التعديل هنا أيضاً ليطابق الـ compileSdk
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
